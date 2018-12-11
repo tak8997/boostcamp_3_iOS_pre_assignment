3c35a0592881cdf13b5341574a9f17cf76464319
Gaon Kim <gaon.kim.dev@gmail.com>
2018-12-12 05:18:47 +0900

    Add `Network` class

---
+++ BoxOffice/Network/Network.swift
@@ -0,0 +1,47 @@
+//
+//  Network.swift
+//  BoxOffice
+//
+//  Created by Gaon Kim on 12/12/2018.
+//  Copyright © 2018 Gaon Kim. All rights reserved.
+//
+
+import UIKit
+
+class Network {
+    static func request(url: URL, handler: @escaping ((Data) -> Void)) {
+        turnOnNetworkIndicator()
+        let session: URLSession = URLSession(configuration: .default)
+        let dataTask: URLSessionDataTask = session.dataTask(with: url) { (data, response, error) in
+            turnOffNetworkIndicator()
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data: Data = data else { return }
+            handler(data)
+        }
+        dataTask.resume()
+    }
+    
+    static func fetchImage(url: URL, handler: @escaping ((Data) -> Void)) {
+        guard let data: Data = try? Data(contentsOf: url) else { return }
+        handler(data)
+    }
+}
+
+extension Network {
+    private static func turnOnNetworkIndicator() {
+        DispatchQueue.main.async {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        }
+    }
+    
+    private static func turnOffNetworkIndicator() {
+        DispatchQueue.main.async {
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        }
+    }
+}
